e2473c8374ebba2bf3fc7f35cf939229f032f105
BrandonMathis <Brandon@KeysetTS.com>
2011-09-22 22:45:07 -0400

    Removed sinatra from bundle

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,8 @@
+require "bundler/capistrano"
+
 load 'deploy' if respond_to?(:namespace) # cap2 differentiator
+
+set :default_environment, {'PATH' => '/home/bemathis/.gems:/usr/lib/ruby/gems/1.8:/home/bemathis/bin:/home/bemathis/.gems/bin:/home/bemathis/.gems/bin:/usr/lib/ruby/gems/1.8/bin/:/usr/local/bin:/usr/bin:/bin:/usr/bin/X11:/usr/games'}
 
 default_run_options[:pty] = true
 
@@ -22,5 +26,9 @@ server domain, :app, :web
 namespace :deploy do
   task :restart do
     run "touch #{current_path}/tmp/restart.txt" 
+  end
+
+  task :symlink do
+    run "ln -nfs #{shared_path}/bundle/ruby #{current_path}/vendor/ruby"
   end
 end
