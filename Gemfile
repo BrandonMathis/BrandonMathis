45a9a3c9019617be1bf8c4c6818cc5571a197827
Brandon Mathis <bemathis@gmail.com>
2015-01-12 15:22:39 -0500

    Update gems font and favicon

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@
 # the following line to use "https"
 source 'http://rubygems.org'
 
-gem "middleman", "~>3.0.14"
+gem "middleman"
 gem "middleman-deploy"
--- config.rb
+++ config.rb
@@ -61,6 +61,8 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+set :font_dir, 'fonts'
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
