912a92d6eb5131056287ffdbd9a75c6f58c84697
Brandon Mathis <bemathis@gmail.com>
2016-02-29 10:30:06 -0500

    Sig

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,2 @@
 //= require_tree .
-console.log('%c 🍥 Sup? 🍥 ' + '%c http://brandonmathis.me ', 'background: #000000; color: #CDCDCD', 'background: #24CF5F; color: #000000');
+console.log('%c  ' + '%c 🍥 Sup? 🍥 ' + '%c  ' + '%c http://brandonmathis.me ' + '%c  ', 'background: #1fdd61', 'background: #000000; color: #CDCDCD', 'background: #1fdd61', 'background: #00ff58; color: #000000', 'background: #1fdd61');
